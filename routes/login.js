3f9b48d48a886eb9212c82ca89df98e1456ec58c
FranciscoGB <franciscogrynberg34@gmail.com>
2021-04-11 13:42:17 -0300

    v0.0.2

--- routes/login.js
+++ routes/login.js
@@ -1,32 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
 
-function checkAuthentication(req,res,next){
-  if (req.isAuthenticated()) {
-    if(req.user.tipo_conta == 'user'){
-      res.redirect('/user')
-    }
-  
-    else if (req.user.tipo_conta == 'admin') {
-      res.redirect('/admin')
-    }
-  }
-  else{
-    next()
-  }
-}
-
-router.get('/', checkAuthentication, function(req, res) {
-  res.render('login',  { message: req.flash('message') })
+router.get('/', function(req, res) {
+  res.render('emconstrucao')
 });
-
-router.post('/', passport.authenticate('users',{successRedirect:'/user', failureRedirect: '/login', failureFlash: true }));
-
-router.get('/admin', checkAuthentication, function(req, res) {
-  res.render('login',  { message: req.flash('message') })
-});
-
-router.post('/admin', passport.authenticate('admin', {successRedirect:'/admin', failureRedirect: '/login/admin', failureFlash: true }));
 
 module.exports = router;
